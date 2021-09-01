e62131cc05c1e86ff0bf894f144419a8c7c91216
vidyasagar gaikwad <vidyasagargaikwad7128@gmail.com>
2021-09-01 15:52:04 +0530

    Added : Switch Case

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -7,10 +7,19 @@ namespace EmployeeWage
         static void Main(string[] args)
         {
             int PER_HOUR = 20;
+            int FULL_TIME = 8;
             int PART_TIME = 4;
             int salary;
+            int rand = new Random().Next(2);
             salary = PER_HOUR * PART_TIME;
-            Console.WriteLine($"Salary is : {salary}");
+            switch (rand) {
+                case 0 :
+                    Console.WriteLine($"Full Time Salary : {PER_HOUR * FULL_TIME}");
+                    break;
+                case 1:
+                    Console.WriteLine($"Part Time Salary : {PER_HOUR * PART_TIME}");
+                    break;
+            }
             
         }
     }
