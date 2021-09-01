174268f039319c0a1a69e539d08e3686a4431760
vidyasagar gaikwad <vidyasagargaikwad7128@gmail.com>
2021-09-01 18:24:48 +0530

    Added : 20 Days Salary

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -9,17 +9,38 @@ namespace EmployeeWage
             int PER_HOUR = 20;
             int FULL_TIME = 8;
             int PART_TIME = 4;
-            int salary;
-            int rand = new Random().Next(2);
-            salary = PER_HOUR * PART_TIME;
-            switch (rand) {
-                case 0 :
-                    Console.WriteLine($"Full Time Salary : {PER_HOUR * FULL_TIME}");
-                    break;
-                case 1:
-                    Console.WriteLine($"Part Time Salary : {PER_HOUR * PART_TIME}");
-                    break;
+            int salary=0;
+            int totalSalary=0;
+            int WORKING_DAYS = 20;
+            for(int i = 0; i <= WORKING_DAYS; i++)
+            {
+                int rand = new Random().Next(3);
+                switch (rand)
+                {
+                    case 0:
+                        salary = PER_HOUR* FULL_TIME;
+                        totalSalary = totalSalary + salary;
+                        Console.WriteLine($"Day {i} Salary : {salary}");
+                        break;
+                    case 1:
+                        salary = PER_HOUR * PART_TIME;
+                        totalSalary = totalSalary + salary;
+                        Console.WriteLine($"Day {i} Salary : {salary}");
+                        
+                        break;
+                    case 2:
+                        salary = 0;
+                        Console.WriteLine($"Day {i} Salary : {salary}");
+                        break;
+                    default: Console.WriteLine(".....");
+                        break;
+                }
+                if (i == 20)
+                {
+                    Console.WriteLine($"20 Days Salary is : {totalSalary}");
+                }
             }
+            
             
         }
     }
